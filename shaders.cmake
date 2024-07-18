5d770d472e4a500d931dafaffacc0e329c609402
UgurkanTech <ugurkanstr@gmail.com>
2024-07-19 02:10:23 +0300

    Many changes

---
+++ shaders.cmake
@@ -0,0 +1,95 @@
+# Shader compilation script for BGFX
+cmake_minimum_required(VERSION 3.15)
+
+# Set the shader output directory
+set(SHADER_OUTPUT_DIR "${CMAKE_SOURCE_DIR}/bin/shaders")
+
+file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/glsl)
+file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/essl)
+#file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/dx12)
+file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/dx11)
+file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/metal)
+file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR}/spirv)
+
+find_program(SHADERC_EXECUTABLE
+    NAMES shaderc
+    PATHS "${CMAKE_SOURCE_DIR}/bin"
+    NO_DEFAULT_PATH
+)
+if(NOT SHADERC_EXECUTABLE)
+    message(FATAL_ERROR "shaderc not found. Make sure it's built and available in the bin directory.")
+endif()
+
+# Initialize
+set(ALL_GENERATED_SHADER_FILES "")
+
+function(compile_shader SHADER_FILE SHADER_TYPE SHADER_SHORT_TYPE)
+    get_filename_component(SHADER_NAME ${SHADER_FILE} NAME_WE)
+    set(VARYING_DEF_FILE "${CMAKE_CURRENT_SOURCE_DIR}/shaders/varying.def.sc")
+
+
+    #TODO:
+    #get_filename_component(SHADER_DIR ${SHADER_FILE} DIRECTORY)
+    #set(VARYING_DEF_FILE "${SHADER_DIR}/${SHADER_NAME}.def.sc")
+
+    if(NOT EXISTS ${VARYING_DEF_FILE})
+        #message(WARNING "Varying definition file not found: ${VARYING_DEF_FILE}")
+        #set(VARYING_DEF_FILE "${SHADER_DIR}/varying.def.sc")
+    endif()
+
+
+    set(COMMON_ARGS
+        -f ${SHADER_FILE}
+        -i ${BGFX_DIR}/src
+        --varyingdef ${VARYING_DEF_FILE}
+        --type ${SHADER_TYPE}
+    )
+
+    # Define output files with different names for vertex and fragment shaders
+    set(OUTPUT_FILES
+        ${SHADER_OUTPUT_DIR}/glsl/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+        ${SHADER_OUTPUT_DIR}/essl/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+        ${SHADER_OUTPUT_DIR}/dx11/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+        #${SHADER_OUTPUT_DIR}/dx12/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+        ${SHADER_OUTPUT_DIR}/metal/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+        ${SHADER_OUTPUT_DIR}/spirv/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin
+    )
+    add_custom_command(
+            OUTPUT ${OUTPUT_FILES}
+            COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/glsl/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform linux   --profile 420
+            COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/essl/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform android --profile 100_es
+            COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/dx11/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform windows --profile s_5_0 -O 3
+            #COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/dx12/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform windows --profile s_5_0 -O 3
+            COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/metal/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform osx --profile metal
+            COMMAND ${SHADERC_EXECUTABLE} ${COMMON_ARGS} -o ${SHADER_OUTPUT_DIR}/spirv/${SHADER_NAME}${SHADER_SHORT_TYPE}.bin --platform linux --profile spirv
+            DEPENDS ${SHADER_FILE} ${VARYING_DEF_FILE}
+            COMMENT "Compiling shader ${SHADER_FILE}"
+            VERBATIM
+        )
+
+     # Add the output files to the global list of generated files
+     set(ALL_GENERATED_SHADER_FILES ${ALL_GENERATED_SHADER_FILES} ${OUTPUT_FILES} PARENT_SCOPE)
+endfunction()
+
+# Find all shader files
+file(GLOB_RECURSE SHADER_FILES 
+    "${CMAKE_CURRENT_SOURCE_DIR}/shaders/*.sc"
+)
+
+# Compile all shaders
+foreach(SHADER ${SHADER_FILES})
+    if(SHADER MATCHES ".*vs\\.sc$")
+        compile_shader(${SHADER} "vertex" ".vs")
+    elseif(SHADER MATCHES ".*fs\\.sc$")
+        compile_shader(${SHADER} "fragment" ".fs")
+    elseif(SHADER MATCHES ".*cs\\.sc$")
+        compile_shader(${SHADER} "compute" ".cs")
+    endif()
+endforeach()
+
+# Create a custom target for shader compilation
+add_custom_target(
+    compile_shaders ALL
+    DEPENDS ${ALL_GENERATED_SHADER_FILES}
+    COMMENT "Compiling all shaders"
+)
--- src/main.cpp
+++ src/main.cpp
@@ -1,230 +1,377 @@
-#include <stdio.h>
-#include <iostream>
-#include <bx/bx.h>
-#include <bx/spscqueue.h>
-#include <bx/thread.h>
-#include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
-#include <GLFW/glfw3.h>
-#if BX_PLATFORM_LINUX
-#define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
-#define GLFW_EXPOSE_NATIVE_WIN32
-#elif BX_PLATFORM_OSX
-#define GLFW_EXPOSE_NATIVE_COCOA
-#define GLFW_EXPOSE_NATIVE_NSGL
-#endif
-#include <GLFW/glfw3native.h>
-#include "imgui_bgfx.h"
+/*
+ * Copyright 2011-2024 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
+ */
 
-static bx::DefaultAllocator s_allocator;
-static bx::SpScUnboundedQueue s_apiThreadEvents(&s_allocator);
+#include "common.h"
+#include "bgfx_utils.h"
+#include "imgui/imgui.h"
 
-enum class EventType
+
+namespace
 {
-    Exit,
-    Key,
-    Resize,
-    ImGuiInit
+
+struct PosColorVertex
+{
+	float m_x;
+	float m_y;
+	float m_z;
+	uint32_t m_abgr;
+
+	static void init()
+	{
+		ms_layout
+			.begin()
+			.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+			.add(bgfx::Attrib::Color0,   4, bgfx::AttribType::Uint8, true)
+			.end();
+	};
+
+	static bgfx::VertexLayout ms_layout;
 };
 
-struct ExitEvent
+bgfx::VertexLayout PosColorVertex::ms_layout;
+
+static PosColorVertex s_cubeVertices[] =
 {
-    EventType type = EventType::Exit;
+	{-1.0f,  1.0f,  1.0f, 0xff000000 },
+	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
+	{-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+	{ 1.0f, -1.0f,  1.0f, 0xff00ffff },
+	{-1.0f,  1.0f, -1.0f, 0xffff0000 },
+	{ 1.0f,  1.0f, -1.0f, 0xffff00ff },
+	{-1.0f, -1.0f, -1.0f, 0xffffff00 },
+	{ 1.0f, -1.0f, -1.0f, 0xffffffff },
 };
 
-struct KeyEvent
+static const uint16_t s_cubeTriList[] =
 {
-    EventType type = EventType::Key;
-    int key;
-    int action;
+	0, 1, 2, // 0
+	1, 3, 2,
+	4, 6, 5, // 2
+	5, 6, 7,
+	0, 2, 4, // 4
+	4, 2, 6,
+	1, 5, 3, // 6
+	5, 7, 3,
+	0, 4, 1, // 8
+	4, 5, 1,
+	2, 3, 6, // 10
+	6, 3, 7,
 };
 
-struct ResizeEvent
+static const uint16_t s_cubeTriStrip[] =
 {
-    EventType type = EventType::Resize;
-    uint32_t width;
-    uint32_t height;
+	0, 1, 2,
+	3,
+	7,
+	1,
+	5,
+	0,
+	4,
+	2,
+	6,
+	7,
+	4,
+	5,
 };
 
-struct ImGuiInitEvent
+static const uint16_t s_cubeLineList[] =
 {
-    EventType type = EventType::ImGuiInit;
-    GLFWwindow* window;
+	0, 1,
+	0, 2,
+	0, 4,
+	1, 3,
+	1, 5,
+	2, 3,
+	2, 6,
+	3, 7,
+	4, 5,
+	4, 6,
+	5, 7,
+	6, 7,
 };
 
-static void glfw_errorCallback(int error, const char *description)
+static const uint16_t s_cubeLineStrip[] =
 {
-    fprintf(stderr, "GLFW error %d: %s\n", error, description);
-}
-
-static void glfw_keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-    auto keyEvent = new KeyEvent;
-    keyEvent->key = key;
-    keyEvent->action = action;
-    s_apiThreadEvents.push(keyEvent);
-}
-
-struct ApiThreadArgs
-{
-    bgfx::PlatformData platformData;
-    uint32_t width;
-    uint32_t height;
+	0, 2, 3, 1, 5, 7, 6, 4,
+	0, 2, 6, 4, 5, 7, 3, 1,
+	0,
 };
 
-static int32_t runApiThread(bx::Thread *self, void *userData)
+static const uint16_t s_cubePoints[] =
 {
-    auto args = (ApiThreadArgs *)userData;
-    bgfx::Init init;
-    init.platformData = args->platformData;
-    init.resolution.width = args->width;
-    init.resolution.height = args->height;
-    init.resolution.reset = BGFX_RESET_VSYNC;
+	0, 1, 2, 3, 4, 5, 6, 7
+};
 
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-	init.type = bgfx::RendererType::Vulkan;
-#elif BX_PLATFORM_OSX
-	init.type = bgfx::RendererType::Metal;
-#elif BX_PLATFORM_WINDOWS
-	init.type = bgfx::RendererType::Direct3D12;
-#endif
-
-    if (!bgfx::init(init))
-        return 1;
-    
-	
-    const bgfx::ViewId kClearView = 0;
-    bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
-    bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-    uint32_t width = args->width;
-    uint32_t height = args->height;
-    bool showStats = true;
-    bool exit = false;
-    
-    float lastTime = 0;
-    float dt;
-    float time;
-
-    bool imguiInitialized = false;
-
-    while (!exit) {
-        time = (float)glfwGetTime();
-        dt = time - lastTime;
-        lastTime = time;
-
-        while (auto ev = (EventType *)s_apiThreadEvents.pop()) {
-            if (*ev == EventType::Key) {
-                auto keyEvent = (KeyEvent *)ev;
-                if (keyEvent->key == GLFW_KEY_F1 && keyEvent->action == GLFW_RELEASE)
-                    showStats = !showStats;
-            }
-            else if (*ev == EventType::Resize) {
-                auto resizeEvent = (ResizeEvent *)ev;
-                bgfx::reset(resizeEvent->width, resizeEvent->height, init.resolution.reset);
-                bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-                width = resizeEvent->width;
-                height = resizeEvent->height;
-                if (imguiInitialized) {
-                    imguiReset(uint16_t(width), uint16_t(height));
-                }
-            }
-            else if (*ev == EventType::ImGuiInit) {
-                auto imguiInitEvent = (ImGuiInitEvent *)ev;
-                imguiInit(imguiInitEvent->window);
-                imguiInitialized = true;
-            }
-            else if (*ev == EventType::Exit) {
-                exit = true;
-            }
-            delete ev;
-        }
-
-        bgfx::touch(kClearView);
-        bgfx::dbgTextClear();
-        bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-        bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-        bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-        bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-        const bgfx::Stats* stats = bgfx::getStats();
-        bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-        bgfx::setDebug(showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
-
-		if (imguiInitialized) {
-			imguiEvents(dt);
-            ImGui::NewFrame();
-            ImGui::ShowDemoWindow();
-			ImGui::EndFrame();
-            ImGui::Render();
-            imguiRender(ImGui::GetDrawData());
-        }
-
-        bgfx::frame();
-    }
-
-    if (imguiInitialized) {
-        imguiShutdown();
-    }
-    bgfx::shutdown();
-    return 0;
-}
-
-int main(int argc, char **argv)
+static const char* s_ptNames[]
 {
-    glfwSetErrorCallback(glfw_errorCallback);
-    if (!glfwInit())
-        return 1;
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(1024, 768, "helloworld multithreaded", nullptr, nullptr);
-    if (!window)
-        return 1;
-    glfwSetKeyCallback(window, glfw_keyCallback);
-    
-    #if BX_PLATFORM_OSX
-    bgfx::renderFrame();
-    #endif
-    
-    ApiThreadArgs apiThreadArgs;
-    #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-    apiThreadArgs.platformData.ndt = glfwGetX11Display();
-    apiThreadArgs.platformData.nwh = (void*)(uintptr_t)glfwGetX11Window(window);
-    #elif BX_PLATFORM_OSX
-    apiThreadArgs.platformData.nwh = glfwGetCocoaWindow(window);
-    #elif BX_PLATFORM_WINDOWS
-    apiThreadArgs.platformData.nwh = glfwGetWin32Window(window);
-    #endif
-    int width, height;
-    glfwGetWindowSize(window, &width, &height);
-    apiThreadArgs.width = (uint32_t)width;
-    apiThreadArgs.height = (uint32_t)height;
+	"Triangle List",
+	"Triangle Strip",
+	"Lines",
+	"Line Strip",
+	"Points",
+};
 
-    bx::Thread apiThread;
-    apiThread.init(runApiThread, &apiThreadArgs);
+static const uint64_t s_ptState[]
+{
+	UINT64_C(0),
+	BGFX_STATE_PT_TRISTRIP,
+	BGFX_STATE_PT_LINES,
+	BGFX_STATE_PT_LINESTRIP,
+	BGFX_STATE_PT_POINTS,
+};
+BX_STATIC_ASSERT(BX_COUNTOF(s_ptState) == BX_COUNTOF(s_ptNames) );
 
-    // Send ImGui initialization event to the API thread
-    auto imguiInitEvent = new ImGuiInitEvent;
-    imguiInitEvent->window = window;
-    s_apiThreadEvents.push(imguiInitEvent);
+class ExampleCubes : public entry::AppI
+{
+public:
+	ExampleCubes(const char* _name, const char* _description, const char* _url)
+		: entry::AppI(_name, _description, _url)
+		, m_pt(0)
+		, m_r(true)
+		, m_g(true)
+		, m_b(true)
+		, m_a(true)
+	{
+		
+	}
 
-    bool exit = false;
-    while (!exit) {
-        glfwPollEvents();
-        if (glfwWindowShouldClose(window)) {
-            s_apiThreadEvents.push(new ExitEvent);
-            exit = true;
-        }
-        int oldWidth = width, oldHeight = height;
-        glfwGetWindowSize(window, &width, &height);
-        if (width != oldWidth || height != oldHeight) {
-            auto resize = new ResizeEvent;
-            resize->width = (uint32_t)width;
-            resize->height = (uint32_t)height;
-            s_apiThreadEvents.push(resize);
-        }
-        bgfx::renderFrame();
-    }
-    while (bgfx::RenderFrame::NoContext != bgfx::renderFrame()) {}
-    apiThread.shutdown();
-    glfwTerminate();
-    return apiThread.getExitCode();
-}
+	void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height) override
+	{
+		Args args(_argc, _argv);
+
+		m_width  = _width;
+		m_height = _height;
+		m_debug  = BGFX_DEBUG_STATS;
+		m_reset  = BGFX_RESET_VSYNC;
+
+		bgfx::Init init;
+		init.type     = bgfx::RendererType::OpenGL;
+		init.vendorId = args.m_pciId;
+		init.platformData.nwh  = entry::getNativeWindowHandle(entry::kDefaultWindowHandle);
+		init.platformData.ndt  = entry::getNativeDisplayHandle();
+		init.platformData.type = entry::getNativeWindowHandleType();
+		init.resolution.width  = m_width;
+		init.resolution.height = m_height;
+		init.resolution.reset  = m_reset;
+		bgfx::init(init);
+
+		// Enable debug text.
+		bgfx::setDebug(m_debug);
+
+		// Set view 0 clear state.
+		bgfx::setViewClear(0
+			, BGFX_CLEAR_COLOR|BGFX_CLEAR_DEPTH
+			, 0x303030ff
+			, 1.0f
+			, 0
+			);
+
+		// Create vertex stream declaration.
+		PosColorVertex::init();
+
+		// Create static vertex buffer.
+		m_vbh = bgfx::createVertexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			  bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices) )
+			, PosColorVertex::ms_layout
+			);
+
+		// Create static index buffer for triangle list rendering.
+		m_ibh[0] = bgfx::createIndexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList) )
+			);
+
+		// Create static index buffer for triangle strip rendering.
+		m_ibh[1] = bgfx::createIndexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			bgfx::makeRef(s_cubeTriStrip, sizeof(s_cubeTriStrip) )
+			);
+
+		// Create static index buffer for line list rendering.
+		m_ibh[2] = bgfx::createIndexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			bgfx::makeRef(s_cubeLineList, sizeof(s_cubeLineList) )
+			);
+
+		// Create static index buffer for line strip rendering.
+		m_ibh[3] = bgfx::createIndexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			bgfx::makeRef(s_cubeLineStrip, sizeof(s_cubeLineStrip) )
+			);
+
+		// Create static index buffer for point list rendering.
+		m_ibh[4] = bgfx::createIndexBuffer(
+			// Static data can be passed with bgfx::makeRef
+			bgfx::makeRef(s_cubePoints, sizeof(s_cubePoints) )
+			);
+
+		// Create program from shaders.
+		m_program = loadProgram("cubes.vs", "cubes.fs");
+
+		m_timeOffset = bx::getHPCounter();
+
+		imguiCreate();
+	}
+
+	virtual int shutdown() override
+	{
+		imguiDestroy();
+
+		// Cleanup.
+		for (uint32_t ii = 0; ii < BX_COUNTOF(m_ibh); ++ii)
+		{
+			bgfx::destroy(m_ibh[ii]);
+		}
+
+		bgfx::destroy(m_vbh);
+		bgfx::destroy(m_program);
+
+		// Shutdown bgfx.
+		bgfx::shutdown();
+
+		return 0;
+	}
+
+	bool update() override
+	{
+		if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState) )
+		{
+			imguiBeginFrame(m_mouseState.m_mx
+				,  m_mouseState.m_my
+				, (m_mouseState.m_buttons[entry::MouseButton::Left  ] ? IMGUI_MBUT_LEFT   : 0)
+				| (m_mouseState.m_buttons[entry::MouseButton::Right ] ? IMGUI_MBUT_RIGHT  : 0)
+				| (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0)
+				,  m_mouseState.m_mz
+				, uint16_t(m_width)
+				, uint16_t(m_height)
+				);
+
+			showExampleDialog(this);
+
+			ImGui::SetNextWindowPos(
+				  ImVec2(m_width - m_width / 5.0f - 10.0f, 10.0f)
+				, ImGuiCond_FirstUseEver
+				);
+			ImGui::SetNextWindowSize(
+				  ImVec2(m_width / 5.0f, m_height / 3.5f)
+				, ImGuiCond_FirstUseEver
+				);
+			ImGui::Begin("Settings"
+				, NULL
+				, 0
+				);
+
+			ImGui::Checkbox("Write R", &m_r);
+			ImGui::Checkbox("Write G", &m_g);
+			ImGui::Checkbox("Write B", &m_b);
+			ImGui::Checkbox("Write A", &m_a);
+
+			ImGui::Text("Primitive topology:");
+			ImGui::Combo("##topology", (int*)&m_pt, s_ptNames, BX_COUNTOF(s_ptNames) );
+
+			ImGui::End();
+
+			imguiEndFrame();
+
+			float time = (float)( (bx::getHPCounter()-m_timeOffset)/double(bx::getHPFrequency() ) );
+
+			const bx::Vec3 at  = { 0.0f, 0.0f,   0.0f };
+			const bx::Vec3 eye = { 0.0f, 0.0f, -35.0f };
+
+			// Set view and projection matrix for view 0.
+			{
+				float view[16];
+				bx::mtxLookAt(view, eye, at);
+
+				float proj[16];
+				bx::mtxProj(proj, 60.0f, float(m_width)/float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+				bgfx::setViewTransform(0, view, proj);
+
+				// Set view 0 default viewport.
+				bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
+			}
+
+			// This dummy draw call is here to make sure that view 0 is cleared
+			// if no other draw calls are submitted to view 0.
+			bgfx::touch(0);
+
+			bgfx::IndexBufferHandle ibh = m_ibh[m_pt];
+			uint64_t state = 0
+				| (m_r ? BGFX_STATE_WRITE_R : 0)
+				| (m_g ? BGFX_STATE_WRITE_G : 0)
+				| (m_b ? BGFX_STATE_WRITE_B : 0)
+				| (m_a ? BGFX_STATE_WRITE_A : 0)
+				| BGFX_STATE_WRITE_Z
+				| BGFX_STATE_DEPTH_TEST_LESS
+				| BGFX_STATE_CULL_CW
+				| BGFX_STATE_MSAA
+				| s_ptState[m_pt]
+				;
+
+			// Submit 11x11 cubes.
+			for (uint32_t yy = 0; yy < 11; ++yy)
+			{
+				for (uint32_t xx = 0; xx < 11; ++xx)
+				{
+					float mtx[16];
+					bx::mtxRotateXY(mtx, time + xx*0.21f, time + yy*0.37f);
+					mtx[12] = -15.0f + float(xx)*3.0f;
+					mtx[13] = -15.0f + float(yy)*3.0f;
+					mtx[14] = 0.0f;
+
+					// Set model matrix for rendering.
+					bgfx::setTransform(mtx);
+
+					// Set vertex and index buffer.
+					bgfx::setVertexBuffer(0, m_vbh);
+					bgfx::setIndexBuffer(ibh);
+
+					// Set render states.
+					bgfx::setState(state);
+
+					// Submit primitive for rendering to view 0.
+					bgfx::submit(0, m_program);
+				}
+			}
+
+			// Advance to next frame. Rendering thread will be kicked to
+			// process submitted rendering primitives.
+			bgfx::frame();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	entry::MouseState m_mouseState;
+
+	uint32_t m_width;
+	uint32_t m_height;
+	uint32_t m_debug;
+	uint32_t m_reset;
+	bgfx::VertexBufferHandle m_vbh;
+	bgfx::IndexBufferHandle m_ibh[BX_COUNTOF(s_ptState)];
+	bgfx::ProgramHandle m_program;
+	int64_t m_timeOffset;
+	int32_t m_pt;
+
+	bool m_r;
+	bool m_g;
+	bool m_b;
+	bool m_a;
+};
+
+} // namespace
+
+ENTRY_IMPLEMENT_MAIN(
+	  ExampleCubes
+	, "01-cubes"
+	, "Rendering simple static mesh."
+	, "https://bkaradzic.github.io/bgfx/examples.html#cubes"
+	);
+
+int _main_(int, char**) { return 0;}
